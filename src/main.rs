8217f30851209332943d89075dbba5d8c2052041
Sehny <sehnoutka.martin@gmail.com>
2017-01-02 17:37:53 +0100

    Modified version for Redox OS.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,30 @@ use libdns::message::{Header, Question, QuestionBuilder, Message};
 use libdns::wire::{FromWire, ToWire};
 
 use std::env;
-use std::net::UdpSocket;
+use std::fs::File;
+use std::io::{Error, Result, Read};
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, UdpSocket};
+use std::str::FromStr;
+
+#[cfg(target_os = "redox")]
+fn get_local_socket() -> Result<SocketAddrV4> {
+
+    println!("I'm running Redox! :-) ");
+
+    let mut ip_string = String::new();
+    File::open("/etc/net/ip")?.read_to_string(&mut ip_string)?;
+    let ip: Vec<u8> = ip_string.trim().split(".").map(|part| part.parse::<u8>()
+                               .unwrap_or(0)).collect();
+    let local_ip = Ipv4Addr::new(ip[0], ip[1], ip[2], ip[3]);
+    Ok(SocketAddrV4::new(local_ip, 0))
+}
+
+#[cfg(not(target_os = "redox"))]
+fn get_local_socket() -> Result<SocketAddrV4> {
+    let local_ip = Ipv4Addr::new(127, 0, 0, 1);
+    Ok(SocketAddrV4::new(local_ip, 0))
+}
+
 
 fn main() {
     let args = env::args();
@@ -14,7 +37,7 @@ fn main() {
 
     // Create a message
     let header = Header {
-        id: 123456,
+        id: 1236,
         response: false,
         opcode: 0,
         aa: false,
@@ -29,7 +52,7 @@ fn main() {
         arcount: 0,
     };
 
-    let question = QuestionBuilder::no_name(1,1).set_name(name.to_string()).finish();
+    let question = QuestionBuilder::default().set_name(name.to_string()).finish();
 
     let query = Message {
         header: header,
@@ -40,14 +63,26 @@ fn main() {
     };
 
     // Send it
-    let mut socket = UdpSocket::bind("127.0.0.1:30000").unwrap();
-    let server = format!("{}:53", server);
-    socket.connect(&server.as_str()).unwrap();
-    socket.send(&query.to_wire()).unwrap();
+    let socket = UdpSocket::bind(get_local_socket()
+                                .expect("Cannot get a local socket"))
+                                .expect("Cannot bind to the local socket.");
+
+    let dns: Vec<u8> = server.trim().split(".").map(|part| part.parse::<u8>()
+                             .unwrap_or(0)).collect();
+    if dns.len() != 4 {
+        println!("Server address is not valid.");
+        return
+    }
+
+    let server = SocketAddrV4::new(Ipv4Addr::new(dns[0], dns[1], dns[2], dns[3]), 53);
+
+    socket.connect(&SocketAddr::V4(server)).expect("Cannot connect to the server");
+    socket.send(&query.to_wire()).expect("Cannot send the query");
 
     // Read a response
-    let mut buffer = [0u8;1024];
-    let (length,_) = socket.recv_from(&mut buffer).unwrap();
+    let mut buffer = [0u8;65536];
+    let _  = socket.recv(&mut buffer).expect("Cannot read from the socket");
+    // let (length,_) = socket.recv_from(&mut buffer).expect("Cannot read from the socket");
 
     let response: Message = Message::from_wire(&buffer).unwrap();
     println!("Address of {} is {:?}", name, response.answer[0].rdata);
